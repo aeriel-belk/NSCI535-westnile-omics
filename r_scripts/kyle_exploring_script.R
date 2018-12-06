53458087cf59d9be43ee0697eb1c48a898322855
Kyle S. Root <kroot27@gmail.com>
2018-12-05 18:55:22 -0700

    Cleaned up metadata figures for species.  Started to add code and verbage to rmarkdown draft document.

--- r_scripts/kyle_exploring_script.R
+++ r_scripts/kyle_exploring_script.R
@@ -1,17 +1,51 @@
 library(readr)
 library(tidyverse)
 library(stringr)
+library(ggthemes)
 
 wn_meta_filtered <- read.csv("metadata/metadata.csv") %>% 
   select(Scripps.ID, Species, Sample.type, Collection.date, County, Latitude, Longitude)
 
-wn_meta_filtered %>% 
-  ggplot() +
-  geom_bar(aes(x = County)) +
-  scale_x_discrete(fct_reorder(County, .desc = TRUE))
+wn_meta_sum_county <- wn_meta_filtered %>% 
+  group_by(County) %>% 
+  summarize(
+    n = n()) %>% 
+  ungroup()
 
-wn_meta_filtered %>% 
+wn_meta_sum_county %>%
+  mutate(County = fct_reorder(County, n)) %>% 
+  ggplot(aes(x = County, y = n)) +
+  geom_point() +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "Samples collected by county", x = NULL, y = "# of samples")
+
+wn_meta_sum_species <- wn_meta_filtered %>% 
+  filter(Sample.type == "Tissue") %>% 
   group_by(Species) %>% 
-  ggplot() +
-  geom_bar(aes(x = Species)) +
-  facet_wrap(~ Sample.type, nrow = 2)
+  summarize(
+    n = n()) %>% 
+  ungroup()
+
+wn_meta_sum_species %>%
+  mutate(Species = fct_reorder(Species, n)) %>% 
+  ggplot(aes(x = Species, y = n)) +
+  geom_point() +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "Samples collected by bird species", x = NULL, y = "# of samples")    
+
+wn_meta_sum_mosquitos <- wn_meta_filtered %>% 
+  filter(Sample.type == "Pooled mosquitos") %>% 
+  group_by(Species) %>% 
+  summarize(
+    n = n()) %>% 
+  ungroup()
+
+wn_meta_sum_mosquitos %>%
+  mutate(Species = fct_reorder(Species, n)) %>% 
+  ggplot(aes(x = Species, y = n)) +
+  geom_point() +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "Samples collected by mosquito species", x = NULL, y = "# of samples")    
